10fb898f829e86788010d2a1c8c72836884e6098
Ramyamani9975 <ramyamanivannan99@gmail.com>
2023-03-08 18:28:29 +0530

    Add project files.

---
+++ Student/Models/Interview Registration.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Student.Models
+{
+    public class Interview_Registration
+    {
+        [Required]
+        public string Name { get; set; }
+        
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+
+        public string Experience { get; set; }
+
+        public string JobRole {get;set;}
+
+        public string City { get; set; }
+
+        
+    }
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+    public enum City
+    {
+        Chennai,
+        Delhi,
+        Kolkata,
+        Bangalore,
+        Mumbai,
+        
+
+    }
+}
